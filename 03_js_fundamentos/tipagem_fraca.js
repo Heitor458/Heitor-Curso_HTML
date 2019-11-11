04f544a0910eb929da1ebdfe32d08c6ec3ae7fd6
Heitor458 <heitormota16@gmail.com>
2019-11-11 19:00:47 -0300

    Adicionando arqivo tipagem_fraca.js com o conteúdo da aula 16.Tipagem Fraca

---
+++ 03_js_fundamentos/tipagem_fraca.js
@@ -0,0 +1,9 @@
+let qualquer = 'legal'
+console.log(qualquer) // legal
+console.log(typeof qualquer) // 3.1516 string o output informa qual é o tipo de variavél nesse caso string
+
+
+qualquer = 3.1516
+console.log(qualquer) // 3.1516
+console.log(typeof qualquer) // 3.1516 string o output informa qual é o tipo de variavél nesse caso number
+console.log(object);
