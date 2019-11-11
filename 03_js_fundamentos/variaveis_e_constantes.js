51fa89e225b9d881f7fd74b46bf311643fe497d7
Heitor458 <heitormota16@gmail.com>
2019-11-11 18:32:36 -0300

    Adicionando arqivo variaveis_e_constantes.js contendo o coconteúdo da aula 15  O Básico de Var, Let e Const

---
+++ 03_js_fundamentos/variaveis_e_constantes.js
@@ -0,0 +1,15 @@
+var nomevariavel = 3
+// Também utilizado para declarar variavel
+var a = 3
+let b = 4
+
+var a = 30
+b = 40
+console.log(a, b) // 30 40
+
+a = 300
+b = 400
+console.log(a, b) // 300 400
+
+const c = 5
+console.log(c)
