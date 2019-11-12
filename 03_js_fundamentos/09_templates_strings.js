9d4fc80cf07d4fcf7c30bdf21f565d561e8e9b77
Heitor458 <heitormota16@gmail.com>
2019-11-12 13:25:14 -0300

    Adicionando 09_tenmplates_strigs.js atualizado !

--- 03_js_fundamentos/09_templates_strings.js
+++ 03_js_fundamentos/09_templates_strings.js
@@ -1,3 +1,13 @@
-const name = "Rebeca"
-const concatenacao = "Olá" + nome + '!'
-const template = `
+const nome = 'Rebeca'
+const concatenacao =' Olá' + nome + '!'
+const template= `
+    Olá
+    ${nome}!` // Faz a interpolação do Texto 
+console.log(concatenacao,template);
+
+
+//Expressoes..
+console.log(`1+1=${1+1}`); // Retorna 1+1 = 2 código utilizado para exibir expressoes númericas 
+
+const up = texto => texto.toUpperCase() 
+console.log(`Ei...${up('Cuidado')}!`); // Retorna Ei CUIDADO !
