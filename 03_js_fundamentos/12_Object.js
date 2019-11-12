d8d2ca78bb23717670c5b882fe80c9c9a3829d63
Heitor458 <heitormota16@gmail.com>
2019-11-12 14:58:19 -0300

    Adicionando arquivo 12_Object com conteúdo da aula 24. Tipos em javaScript:Object

---
+++ 03_js_fundamentos/12_Object.js
@@ -0,0 +1,18 @@
+const prod1 = {}
+prod1.nome ='Samsung J7 Prime'
+prod1.preco='1.095'
+prod1['Desconto Show'] = 0.40 //Evitar Atributos com Espaço
+
+console.log(prod1);
+
+const prod2 = {
+    nome:'Iphone',
+    preco:'Um Rim',
+
+}
+console.log(prod2);
+
+
+
+
+
