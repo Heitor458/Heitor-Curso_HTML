ae42e291931aa2fe0ced48db617763b797f476c8
Heitor458 <heitormota16@gmail.com>
2019-11-12 13:03:25 -0300

    Renomeando nome dos arquivos

--- 03_js_fundamentos/06_number_alguns_cuidados.js
+++ 03_js_fundamentos/06_number_alguns_cuidados.js
@@ -1,6 +1,7 @@
 console.log(7 / 0) // infinit
 
 console.log('10.2' / 2) // Retorna 5.1
+console.log('3'+2); // não irá somar e irá retornar 32 
 console.log('show!' * 2) // Gera um NaN = Not a Number
 console.log(0.1 + 0.7) // Não retorna 0.8 pois p JS não suporta a especificação gerando uma imprecisão 
 
---
+++ 03_js_fundamentos/08_strings.js
@@ -0,0 +1,24 @@
+const escola = "Cod3r"
+
+console.log(escola.charAt(4)); //Retorna o character no Indice 4
+console.log(escola.charAt(5)); //Retorna um valor vazio ou Nulo 
+console.log(escola.charCodeAt(3));// Retorna o valor do código relacionado a tabela Unicode
+console.log(escola.indexOf('3')); // Retorna o indice do Digito indicado 
+
+console.log(escola.substring(1)); // Retorna no console as strigns de Index 1 até o final
+console.log(escola.substring(0.3)); // Retorna no console as strings de Index 0 até o Index 3 sem incluir o Index 3.
+
+console.log('Escola '.concat(escola).concat("!"));
+console.log(escola.replace(3,'e')); // Faz a substituição do '3' pela letra 'e'.
+
+console.log('Ana,Maria,Pedro'.split(',')); //Retorna a divisão das Strings por ','
+console.log('Ana,Maria,Pedro'.split(/./));//Retorna a divisão das Strings por '.' Usando AJAX
+
+
+
+
+
+
+
+
+
