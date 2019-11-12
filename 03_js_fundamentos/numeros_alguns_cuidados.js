3e70f11fcfc19bf1b68629575218cd59f2bfe894
Heitor458 <heitormota16@gmail.com>
2019-11-12 11:23:29 -0300

    Adicionando arquivo numeros_alguns_cuidados.js com o conteúdo da aula 18. Number Alguns Cuidados.

---
+++ 03_js_fundamentos/numeros_alguns_cuidados.js
@@ -0,0 +1,11 @@
+console.log(7 / 0) // infinit
+
+console.log('10.2' / 2) // Retorna 5.1
+console.log('show!' * 2) // Gera um NaN = Not a Number
+console.log(0.1 + 0.7) // Não retorna 0.8 pois p JS não suporta a especificação gerando uma imprecisão 
+
+//console.log(10.toString()); //Não pode chamar o literal 
+ 
+console.log((10.345).toFixed(2)); // Consegue chamar o Literal dentro de parenteses e chamar a função!
+
+
