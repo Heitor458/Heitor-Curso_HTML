2de9f011aeaa38e8ba6b733b4706d0a9daa58855
Heitor458 <heitormota16@gmail.com>
2019-11-12 17:29:38 -0300

    Adicionando o arquivo 14_funcao_em_quase_tudo.js com o conteúdo da aula 26.Quase Tudo é Funçãogit status!

---
+++ 03_js_fundamentos/14_funcao_em_quase_tudo.js
@@ -0,0 +1,9 @@
+console.log(typeof Object); // É uma function 
+
+class produto {} 
+console.log(typeof produto);
+
+/* Nas Funções podemos encontrar outras funções,
+ou atributos, pode armazenar uma função como variável,
+pode passar uma função como parametro */
+
