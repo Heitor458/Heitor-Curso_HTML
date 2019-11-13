edde269d5537378563b10695fb51e125e6ba2e9e
Heitor458 <heitormota16@gmail.com>
2019-11-13 13:20:42 -0300

    Adicionando arquivo 20_usando_var_em_loop01.js com o Conteudo da aula 32. Usando Var em Loop#01

---
+++ 03_js_fundamentos/20_usando_var_em_loop01.js
@@ -0,0 +1,12 @@
+for( var i = 0; i < 10; i++){
+    console.log(i);
+}
+console.log('i = ', i);
+
+// Exemplo de bloco de repetição FOR 
+
+/* Montando um FOR 
+for(var i = 0;). Aqui declaramos uma variavel
+for(var i = 0; i < 10) Em seguida  separados por ';' colocamos uma condição de repetição 
+for (var i = 0 ; i < 10 ; i++) por fim Colocamos um incremento para ser executado
+ console.log('i = ', i) Por Ultimo exibimos o resultado no console */
