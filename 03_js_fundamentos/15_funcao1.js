1579f0bdf8eb56aea7c531d837b301b477146d7d
Heitor458 <heitormota16@gmail.com>
2019-11-13 10:25:13 -0300

    deletando arquivo 13_null_e_undefined.js e adicionando arquivo 03_funcao.js

---
+++ 03_js_fundamentos/15_funcao1.js
@@ -0,0 +1,6 @@
+/* Função é como una ação que irá executar
+Uma Sentença de Código.
+A função define um bloco, os blocos são delimitados por chaves,
+então você tem a função em seguida  o corpo da função que são os blocos,
+no qual tem todas a sentença de códigos que será executada quando você chamar a função.
+*/
