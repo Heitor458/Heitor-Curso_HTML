2236cdfe6d30db50509b7f14d9043a7c8afed7dc
Heitor458 <heitormota16@gmail.com>
2019-11-13 13:42:37 -0300

    Atualizando o arquivo 21_usando_let_em_loop01.js

--- 03_js_fundamentos/21_usando_let_em_loop01.js
+++ 03_js_fundamentos/21_usando_let_em_loop01.js
@@ -2,3 +2,9 @@ for(let i = 0; i < 10; i++){
     console.log(i);
 }
 console.log('i = ',i );
+
+/*
+Irá me retornar que console.log('i = ',i); dizendo que i não foi defenido "i is not defined"
+pois estamos declarando a variavel com a palavra reservada let a a variavel só estara visivel
+dentro do bloco for.
+*/
