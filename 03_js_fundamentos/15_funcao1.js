1b7fbd7517ddc8aa3ead367fc322db7200f996d9
Heitor458 <heitormota16@gmail.com>
2019-11-13 11:25:12 -0300

    Aatualizando arquivo 15_funcao01

--- 03_js_fundamentos/15_funcao1.js
+++ 03_js_fundamentos/15_funcao1.js
@@ -3,4 +3,24 @@ Uma Sentença de Código.
 A função define um bloco, os blocos são delimitados por chaves,
 então você tem a função em seguida  o corpo da função que são os blocos,
 no qual tem todas a sentença de códigos que será executada quando você chamar a função.
+Existem Funções anonimas que são funções sem nome.( não irá aparecer nessa aula.)
 */
+ 
+//Função sem Retorno
+function ImprimirSoma(a,b){
+    console.log(a + b);
+ }
+//Modos de passar parâmetros
+ ImprimirSoma()
+ ImprimirSoma(2,3)
+ ImprimirSoma(1,2,3,4,5,6,7,8)
+
+ //Função com Retorno
+ function soma(a , b = 0){
+     return a + b
+ }
+ console.log(soma(2,3));
+ console.log(soma(2)); // Retona dois pois declarei que por padrão o valor da variável b é igual a zero.
+ 
+ 
+ 
