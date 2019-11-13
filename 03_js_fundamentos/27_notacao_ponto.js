7ea1032ded71d1bd273a3fa8c9328d11ddc5df19
Heitor458 <heitormota16@gmail.com>
2019-11-13 16:50:37 -0300

    Adicionando arquivo 27_notacao_ponto.js com conteudo da aula 39. Ntação Ponto

---
+++ 03_js_fundamentos/27_notacao_ponto.js
@@ -0,0 +1,22 @@
+console.log(Math.ceil(6.1)); // Arredondar para cima 
+
+const obj1 = {}
+obj1.nome = 'Bola'
+obj1.tamanho = 'Grande'
+console.log(obj1.nome);
+
+function Obj(nome) {
+    this.nome = nome // usando o this.nome = nome deixa o nome Publico para quem instanciar o objeto Obj.
+    this.exec = function () {
+        console.log('Exec...');
+
+    }
+}
+
+
+const obj2 = new Obj('Cadeira')
+const obj3 = new Obj('Mesa')
+
+console.log(obj2.nome);
+console.log(obj3.nome);
+//notção ponto é uma forma que tenho acesso os  mmenbros de um Ojeto  ou de uma função apartir de notação ponto.
