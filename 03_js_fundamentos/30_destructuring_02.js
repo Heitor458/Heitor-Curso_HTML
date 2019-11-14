bfc6842f85ca1b2d633d98c4c7340e9ceb17e90f
Heitor458 <heitormota16@gmail.com>
2019-11-14 17:22:51 -0300

    Adicionando 30_destructuring_02.js com conteudo da aula 43.Operadores: Destructuring #03

---
+++ 03_js_fundamentos/30_destructuring_02.js
@@ -0,0 +1,11 @@
+// Preocesso de Operadores Destructing usando Arrays 
+const [a] = [10]
+console.log(a);
+
+// Forma de extrair ou desestruturar e atribuir várias variaveis com uma única atribuição 
+const[n1, , n3, ,n5, n6 = 0 ] =[10,7,9,8] 
+console.log(n1,n3,n5,n6);
+
+
+
+
