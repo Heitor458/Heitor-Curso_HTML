6902cef9afca4f1a3c2919705fcacaad80b6eca6
Heitor458 <heitormota16@gmail.com>
2019-11-14 15:51:11 -0300

    Adicionanodo arquivo 24_atribuicao.js com Conteúdo da aula 40.Operadores:Atribuição

---
+++ 28_atribuicao.js
@@ -0,0 +1,20 @@
+// Operadores de Atribuição 
+const a = 7
+let b = 3
+
+b += a //Fazendo atribuição aditiva fazendo um acréscimo  b = b + a
+console.log(b);
+
+b -= 4 // Fazendo Atribuição subtrativa b= b - a
+console.log(b);
+
+b *= 2 //Fazendo uma atribuição de Múltiplicação b = b * a
+console.log(b);
+
+b /= 2 //Fazendo uma atribuição de Divisão b = b/a
+console.log(b);
+
+b %= 2 // Fazendo uma opereção modular b = b %  módulo é o resto inteiro da divisão 
+console.log(b);
+
+
