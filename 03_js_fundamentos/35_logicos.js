069214036dfa4a96527e8617ecf81467eb4f24e3
Heitor458 <heitormota16@gmail.com>
2019-11-18 19:11:48 -0300

    Atualizando o arquivo 35_logicos.js com conteudo referente a aula 47.Operadores:Lógicos

--- 03_js_fundamentos/35_logicos.js
+++ 03_js_fundamentos/35_logicos.js
@@ -16,4 +16,21 @@
    !v -> f  // operadores unários são: Aqueles usados em expressões com apenas 1 operando
    !f -> v 
    */
+function compras(trabalho1, trabalho2) {
+    const comprarsorvete = trabalho1 || trabalho2
+    const comprarTv50 = trabalho1 && trabalho2
+    // const comprarTv32 = !!(tabalho1^trabalho2)//bitwase xor opera bit a bit 
+    const comprarTv32 = trabalho1 != trabalho2 // O ou exclusivo pode ser simulado com a diferença: pois verdadeiro é  diferente de falso é um ou exclusivo falso é diferente de verdadeiro é um ou exclusivo 
+    const mantersaudavel = !comprarsorvete // Operador unário 
+    
+    return { comprarsorvete, comprarTv32, comprarTv50, mantersaudavel } //Criando um Objeto  chave valor (só que implicito )para retonar os valores das const.
+}
+
+console.log(true, true);
+console.log(compras(true, false));
+console.log(compras(true,false));
+console.log(compras(false,true));
+console.log(compras(false,false));
+
+
 
