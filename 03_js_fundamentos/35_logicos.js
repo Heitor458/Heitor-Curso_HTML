69d2a432342f0d49840a00187ee070a2b1e859c0
Heitor458 <heitormota16@gmail.com>
2019-11-18 16:24:17 -0300

    Adicionando arquivo 35_logicos.js com conteudo da aula 47.operadores Lógicos

---
+++ 03_js_fundamentos/35_logicos.js
@@ -0,0 +1,19 @@
+/* Todos os Operandos tem que ser Verdadeiro para que o resultado seja Verdadeiro basta um ser falso para o resultado seja falso
+   v e v -> v
+   v e f -> f 
+   f e ? -> f 
+
+   v ou ? -> v
+   f ou v -> v
+   f ou f -> f 
+
+   Ou Exclusivo  obrigatoriamente od dois tem que ser diferente para que o resultado seja Verdadeiro 
+   v xor v -> f
+   v xor f -> v 
+   f xor v -> f 
+   
+   Negação Lógica 
+   !v -> f  // operadores unários são: Aqueles usados em expressões com apenas 1 operando
+   !f -> v 
+   */
+
