8247bdb503085fc897f84cc7e1c930d5710b7dda
Heitor458 <heitormota16@gmail.com>
2019-11-19 17:24:23 -0300

    Atualizando arquivo 36_unarios.js com conteudo da aula 48.Operadores:Unários

--- 03_js_fundamentos/36_unarios.js
+++ 03_js_fundamentos/36_unarios.js
@@ -1,2 +1,16 @@
-console.log('Teste');
+let num1 =1
+let num2 =2
 
+num1++ //Exemplo de operador Unário de forma 
+console.log(num1);
+--num1 // Forma Prefixada
+console.log(num1);
+
+
+console.log(++num1 === num2--);
+//True or False ?
+console.log(++num1 === num2); 
+/* True pois da seguinte forma que foi escrito o código será exeutado:
+ acrescenta +1 no num1 >>
+  compara com o num 2 , após a comparação 
+  mude o resultado de num 2*/
