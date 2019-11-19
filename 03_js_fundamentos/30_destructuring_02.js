b75221d1c83b7fcd5af4ac3428d0249c408be564
Heitor458 <heitormota16@gmail.com>
2019-11-19 16:51:22 -0300

    Atualizando arquivo 30_destructuring_02.js

--- 03_js_fundamentos/30_destructuring_02.js
+++ 03_js_fundamentos/30_destructuring_02.js
@@ -5,7 +5,3 @@ console.log(a);
 // Forma de extrair ou desestruturar e atribuir várias variaveis com uma única atribuição 
 const[n1, , n3, ,n5, n6 = 0 ] =[10,7,9,8] 
 console.log(n1,n3,n5,n6);
-
-
-
-
