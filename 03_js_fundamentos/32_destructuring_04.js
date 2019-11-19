cc4a61dbddac9fba0a8fc2f371e4701b5133e7d7
Heitor458 <heitormota16@gmail.com>
2019-11-19 16:52:31 -0300

    Atualizando arquivo  32_destructuring_04.js

--- 03_js_fundamentos/32_destructuring_04.js
+++ 03_js_fundamentos/32_destructuring_04.js
@@ -9,3 +9,5 @@ console.log(rand([992])); //Passando apenas o valor Minimo
 console.log(rand([,10])); // Passando apenas o Valor Máximo e vai assumir o minimo como zero 
 console.log(rand([])); // Passando um Array Vazio
 console.log(rand()); // Vai passar um erro como undefined pois não da para desestutuar elementos de algo qu eé nulo ou não definido 
+
+// Links para saber mais sobre destructuring:  https://medium.com/@1sherlynn/javascript-es6-array-object-destructuring-23da62822ccd
