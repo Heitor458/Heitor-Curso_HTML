7b0f081486e954b834d3799fc7c334f17db92412
Heitor458 <heitormota16@gmail.com>
2019-11-22 13:54:51 -0300

    Adicionando 08_for.js com conteudo da aula 60.Usando Estrutura FOR

--- 04_js_Estrutura_de_controle/08_for.js
+++ 04_js_Estrutura_de_controle/08_for.js
@@ -11,8 +11,25 @@ for( let i = 1;i <=10;i++){
 }
 console.log("Fim contador");
 
+// Exemplo de for utilizando Array
 const notas = [6.7,7.4,9.8,8.1,7.7]
 for(let  i = 0 ; i < notas.length; i++){
     console.log(`nota = ${notas[i]}`);
 }
 
+const nota = [6.7,7.4,9.8,8.1,7.7]
+for(i  in nota){
+    console.log(i,nota[i]); 
+}
+
+// Exemplo de for com Objeto Pessoa 
+const pessoa ={
+    nome:'Fulano',
+    sobrenome:'De tal',
+    idade:25,
+    peso: 64
+}
+
+for( let atributo in pessoa) {
+    console.log(`${atributo} = ${pessoa[atributo]}`);
+}
