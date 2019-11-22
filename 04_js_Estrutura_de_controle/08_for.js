35d8bd0102423be4a4308555f333bd8900e46cfb
Heitor458 <heitormota16@gmail.com>
2019-11-22 11:30:05 -0300

    Adicionando arquivo 08_for.js com conteudo da aula 60.Usando a Estrutura FOR

---
+++ 04_js_Estrutura_de_controle/08_for.js
@@ -0,0 +1,18 @@
+// Exemplo usando FOR fazendo um Contador 
+let contador = 1
+while(contador <= 10){
+    contador++
+    console.log(`Contador =  ${contador}.`);
+}
+console.log("Fim Contador");
+
+for( let i = 1;i <=10;i++){
+    console.log(`i = ${contador}.`);
+}
+console.log("Fim contador");
+
+const notas = [6.7,7.4,9.8,8.1,7.7]
+for(let  i = 0 ; i < notas.length; i++){
+    console.log(`nota = ${notas[i]}`);
+}
+
