d7e3f6abd76dc1e2f8a13fc3a421e95881c820f5
Heitor458 <heitormota16@gmail.com>
2019-11-26 11:07:40 -0300

    Atualizando 03_oarametros com o conteudo da aula 65.Parametros e Variaveis

--- 05_funcao.js/03_parametro.js
+++ 05_funcao.js/03_parametro.js
@@ -5,10 +5,11 @@ function soma (){
     }
     return soma
 }
-
+// Passando parametros 
 console.log(soma());
 console.log(soma(1));
 console.log(soma(1.1,2.2,3.3));
+
 console.log(soma(1.1,2.2,'TESTE'));
 console.log(soma('a','b','c'));
 
