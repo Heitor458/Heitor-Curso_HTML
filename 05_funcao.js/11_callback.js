5fcca4bfa023313511635a864c0f672b98ad06d8
Heitor458 <heitormota16@gmail.com>
2019-11-26 19:22:29 -0300

    Adicionando arquivo 11_callback.js com conteudo da aula 74.Funções Callback #01

--- 05_funcao.js/11_callback.js
+++ 05_funcao.js/11_callback.js
@@ -1,8 +1,10 @@
-const fabricantes = ["Mercedes","Audi","BMW"]
+const fabricantes = ["Mercedes", "Audi", "BMW"]
 
-function imprimir(nome,indice){
-    console.log(`${indice+1}.${nome}`);
+function imprimir(nome, indice) {
+    console.log(`${indice + 1}.${nome}`);
 }
 fabricantes.forEach(imprimir)
 
 // O callback é uma função que essa função sera chamada quando um evento acontecer , nesse caso é um loop que é cada um dos elementos precorridos no Array 
+
+
