68a3c1606c2087c9c268b9e4ec20e33005617b27
Heitor458 <heitormota16@gmail.com>
2019-11-26 14:20:55 -0300

    Adicionando arquivos 05_this_bind.js com conteudo da aula 66.Parametros Padrao

--- 05_funcao.js/05_this_bind.js
+++ 05_funcao.js/05_this_bind.js
@@ -11,7 +11,7 @@ pessoa.falar()
 const falar = pessoa.falar
 falar() //conflito entre paradigmas: Funcional e OO
 
-const falarDePessoa = pessoa.falar.bind(pessoa) //Para resolver o conflito entre Paradigma com Bind
+const falarDePessoa = pessoa.falar.bind(pessoa) 
 falarDePessoa();
 
 //  A ideia do Bind utilizado para resolver o problema do this em chamadas multiplas 
