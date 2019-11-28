9f5503f282bc4635fa44c0ad898419a4b98129f6
Heitor458 <heitormota16@gmail.com>
2019-11-28 13:30:16 -0300

    Adicionando arquivo 13_callback_03 com conteudo da aula 76.Funções Callback #03

---
+++ 05_funcao.js/13_callback_03.js
@@ -0,0 +1,6 @@
+// Exemplo de callback no Browser não executa aqui no VScode e sim no Browser 
+document.getElementsByTagName('body')[0].onclick = function(e) {
+    console.log('O Eveno Ocorreu !');
+    
+}
+
