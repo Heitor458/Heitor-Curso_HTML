b7236b5d5ac25cc5f98ff78626f783efa742b6eb
Heitor458 <heitormota16@gmail.com>
2019-11-28 16:42:57 -0300

    Adicionando Arquivo 17_funcao_factory.js com conteudo da aula 81. Funções Factory #01 e 82.Funções Factory #02

--- 05_funcao.js/17_funcao_factory.js
+++ 05_funcao.js/17_funcao_factory.js
@@ -13,10 +13,10 @@ const prod2 = {
 
 // Exemplo com Factory Simples 
 
-function criarPessoa(){
-    return{
-        nome:'Ana' ,
-        sobrenome:'Carolina ',
+function criarPessoa() {
+    return {
+        nome: 'Ana',
+        sobrenome: 'Carolina ',
 
     }
 
@@ -24,14 +24,37 @@ function criarPessoa(){
 console.log(criarPessoa());
 
 // Exemplo da aula  82.Função Factory #02
-function cirarProduto(nome,preco,lancamento,) {
+function criarProduto(nome, preco, lancamento, ) {
     return {
-        nome,preco,lancamento,
+        nome, preco, lancamento,
+        desconto: 0.1,
+        setPreco: function (p) {
+            this.preco = p
+        }
+    }
+}
+console.log(criarProduto('NooteBook', 2199.49, '25/11/2019'));
+console.log(criarProduto('Iphone 11', 5199.6525, '01/11/2020'));
+
+
+function criaProduto1Real(nome, lancamento) {
+    // jeito 1 - refaz o corpo
+    return {
+        nome,
+        preco: 1.00,
+        lancamento,
         desconto: 0.1
     }
-} console.log(cirarProduto('NooteBook',2199.49,'25/11/2019'));
-console.log(cirarProduto('Iphone 11', 5199.6525,'01/11/2020'));
 
+    // jeito 2 - reutiliza outras funcoes
+    return criarProduto(nome, 1.00, lancamento)
+
+    // jeito 3
+    let produto = criarProduto(nome, 2.00, lancamento)
+    produto.preco = 1
+    produto.setPreco(1)
+    return produto
+}
 
 
 
