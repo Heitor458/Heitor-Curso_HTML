e44d8bd203e0af28a742aa10d8e11c530bfccc6b
Heitor458 <heitormota16@gmail.com>
2019-11-29 16:30:59 -0300

    Adicionando arquivo  01_introducao_OO.js com conteudo da aula  87.Introdução à OO

---
+++ 06_JavaScript_Objeto.js/01_introducao_OO.js
@@ -0,0 +1,27 @@
+//  Código não executavel 
+
+
+/*  Paradigma Procedural: Baseado em Procedimentos , 
+Função assim dando nomes para ele e reutilizando em vários lugares 
+estruturada como por exemplo o Paradigma de Orientado a Objeto.
+Com concetos de que funções manipulam dados */
+
+processamento(v1,v2,v3)
+
+// OO: na OO Temos dados e dentro desses daddos temos funções sege o exemplo abaixo :
+
+objeto = {
+    valor1,
+    valor2,
+    processamento(){
+        // comportamentos dentro desse Objeto 
+    }
+}
+//  Assim tendo Objetos que possui valores  e tem funções e  passa invocar esse processamento através de dados.
+/* O Objeto irá funcionar como uma capsula que irá agrupar o comportamentos e atributos */
+/* Principios Importantes :
+1 -- > Abstração
+2 -- > Encapsulamento 
+3 -- > Herança() com a relaão 'é um'  Priorizar a composição ao ivés da Herança  
+4 -- > Polimorfismo 
+*/
