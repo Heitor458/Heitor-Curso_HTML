a0e2111d94c75a18aebb7740c61b455b441b9d9a
Heitor458 <heitormota16@gmail.com>
2019-12-04 16:32:19 -0300

    Adicionando arquvo  08_map_03.js com conteudo da aula 111.Map #03

---
+++ 07_js_Array.js/08_map_03.js
@@ -0,0 +1,22 @@
+// o código abaixo retrata de como funciona o map pode debaixo dos panos 
+Array.prototype.map2 = function (callback) {
+    const newAray = []
+    for (let i = 0; i < this.length; i++) {
+        newAray.push(callback(this[i], i, this))
+    }
+    return newAray
+}
+
+const carrinho = [
+    '{"Nome":"Produto","preco": 3.45}',
+    '{"Nome":"Produto","preco": 3.45}',
+    '{"Nome":"Produto","preco": 3.45}',
+    '{"Nome":"Produto","preco": 3.45}'
+]
+
+// Retoarnar apenas o Preco 
+const paraObjeto = json => JSON.parse(json)
+const apenaspreco = produto => produto.preco
+
+const resultado = carrinho.map2(paraObjeto).map2(apenaspreco)
+console.log(resultado);
