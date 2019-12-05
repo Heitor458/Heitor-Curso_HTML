5fbf2da4d58f7293c4452396c033556d0633c87f
Heitor458 <heitormota16@gmail.com>
2019-12-05 16:28:15 -0300

    Adicionando arquivo 11_reduce_01.js com conteudo da aula 114.Reduce #01

--- 07_js_Array.js/11_reduce_01.js
+++ 07_js_Array.js/11_reduce_01.js
@@ -3,7 +3,22 @@ Reduzir um array  Ele iterará por cada elemento dessa lista com o objetivo
 de ao final gerar um único valor (de qualquer tipo), como por exemplo a soma de todos os elementos desse array. 
 Lembrando que não ficamos presos apenas a números. */
 
-// O EXEMPLO ABAIXO NÃO PERTENCE AO CONTEUDO DA AULA FOI RETIRADO DO: https://medium.com/@raullesteves/javascript-entendendo-o-reduce-de-uma-vez-por-todas-c4cbaa16e380
+
+const alunos = [
+    { nome: 'Cassio', nota: 7.3, bolsista: false },
+    { nome: 'Maria', nota: 9.2, bolsista: true },
+    { nome: 'pedro', nota: 9.8, bolsista: false },
+    { nome: 'Ana', nota: 8.7, bolsista: true },
+]
+
+console.log(alunos.map(a => a.nota));
+const resultado = alunos.map(a => a.nota).reduce(function (acumulador, atual) {
+    console.log(acumulador, atual);
+    return acumulador + atual
+}, 10) // aqui estou passando o valor inicial 
+console.log(resultado);
+
+
 var arrray = [0, 1, 2, 3, 4, 5, 6];
 var total = arrray.reduce((acumulador, numero, indice, original) => {
     console.log(`${acumulador} Total até o momento `);
@@ -18,35 +33,5 @@ var ab = a.reduce((acumulador, elemento) => acumulador += elemento,
 console.log(ab);
 console.warn('Acabou');
 console.log(total);
-/* Exemplo de Cachorro Gato */
-/* Utilizando o filter (pra filtrar, duh), o map (para realizar o calculo em cada elemento) e o reduce (para somarmos tudo) */ 
-const animais = [
-    {
-        nome: 'Fumaca',
-        idade: 3,
-        tipo: 'Cachorro'
-    },
-    {
-        nome: 'Tobby',
-        idade: 6,
-        tipo: 'Cachorro'
-    },
-    {
-        nome: 'Rock',
-        idade: 1,
-        tipo: ' Gato'
-    },
-    {
-        nome: 'Nutella',
-        idade: 3,
-        tipo:'Cachorro'
 
-    },
-];
-
-idadeCachorro = animais.filter((animais) => animais.tipo === 'Cachorro').map((Cachorro) => Cachorro.idade*7).reduce((total,Cachorro) => total += Cachorro)
-idadeCachorro = animais.reduce((total,elemento) => {
-    if (elemento.tipo === 'Cachorro') return total += (elemento.idade *= 7);
-    else return total },0
-)
-
+//Veja outros exemplo em: https://medium.com/@raullesteves/javascript-entendendo-o-reduce-de-uma-vez-por-todas-c4cbaa16e380
