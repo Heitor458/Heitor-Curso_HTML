d55a4126d02eb8b6e133ad79bc5f356a3751c82c
Heitor458 <heitormota16@gmail.com>
2019-12-05 17:58:22 -0300

    Adicionando arquivo 15_concat.js com conteudo da aula 118.Concat

---
+++ 07_js_Array.js/15_concat.js
@@ -0,0 +1,7 @@
+const filhas = ['Camila','Emily']
+const filhos = ['Uoxinton','Uesclei']
+const todos = filhos.concat(filhos,'Fulano')
+console.log(todos,filhas,filhos);
+
+
+console.log([].concat([1,2],[3,4],5,[[6,7]]));
