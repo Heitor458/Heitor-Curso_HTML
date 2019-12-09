2cd1449cecc2c465f9df1438e50cf96d46761fcb
Heitor458 <heitormota16@gmail.com>
2019-12-09 16:15:18 -0300

    Adicionando arquivos 02_moduloA, 02_moduloB.js, 02_moduloCliente.js 03_usando_modulos_terceiros.js, packae-lock.json com Conteudo da aual 121.Sistema de Módulos e 122.Usando Módulos Tercei ros

--- 08_js_Node/03_usando_modulos_terceiros.js
+++ 08_js_Node/03_usando_modulos_terceiros.js
@@ -1,2 +1,9 @@
 const _ = require('lodash')
 setInterval(() => console.log(_.random(1, 1000)), 2000);
+
+/* Nessa Aula instalamos o lodash
+usando o comando : npm i lodash  no caminho c:\laragon\www\curso_web_moderno\08_js_Node>*/
+/* Também instalamos o nodemon no caminho Global usando o comando : npm i -g nodemon
+(A Flag -g é para indicar que ele será instalado de uma forma Global para que ele seja executado direto no terminal)
+com o Nodemon eu consigo alterar o Código  enquanto ele esta sendo executado e ele irá rodar a versão mais atual do código
+apartir do momento que ele for restartado ou Refresh */
