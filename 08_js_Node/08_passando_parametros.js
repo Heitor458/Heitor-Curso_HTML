ba7b4eb6c54e8c85e0cfcc0b4e0066ab36e615f0
Heitor458 <heitormota16@gmail.com>
2019-12-15 17:31:28 -0300

    Adicionando os arquivos 08_passando_parametros_cliente.js e 08_passando_parametros.js conteudo da aula 130.passando parametros entre modulos

--- 08_js_Node/08_passando_parametros.js
+++ 08_js_Node/08_passando_parametros.js
@@ -2,4 +2,4 @@ module.exports = function (...nomes) {
     return nomes.map(nome => `Boa Semana ${nome}!`)
 }
 // essa função irá tranformar uma lista de nomes em uma lista de saudações
-// os "..." nos parametros da função  é um argumento variavel wue você podera passar quantos nomes você quiser !
+// os "..." nos parametros da função  é um argumento variavel wue você podera passar quantos nomes você quiser !!
