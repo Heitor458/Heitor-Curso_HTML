b34e32a763c6f1ea47e2e8884f09c8161e7d0954
Heitor458 <heitormota16@gmail.com>
2019-12-15 17:17:11 -0300

    Renomeando arquivos 09_passando_parametros.js e 09_passando_parametros_clientes.js com conteudo da aula 130.Passando Parametros

--- 08_js_Node/09_passando_parametros_cliente.js
+++
@@ -1,3 +0,0 @@
-const saudacoes = require('./09_passando_parametros')('Ana', 'Lucas', 'Joao', 'Pedro')
-console.log(saudacoes);
-// Espera Retornar [boa semana Ana! , boa semana Lucas! ,boa semana joao!, boa semana Pedro! ]
