370f2a5ca9758038b7c706a214cbe78838dd9a14
Heitor458 <heitormota16@gmail.com>
2019-12-16 10:10:20 -0300

    Adicionando arquivo 08_passando_parametro.js e 08_passndo_parametros_cliente.js com conteudo da aula 130. Passando Parametros entre MModulos

--- 08_js_Node/08_passando_parametros_cliente.js
+++ 08_js_Node/08_passando_parametros_cliente.js
@@ -1,3 +1,3 @@
 const saudacoes = require('./08_passando_parametros')('Ana', 'Lucas', 'Joao', 'Pedro')
 console.log(saudacoes);
-// Espera Retornar [boa semana Ana! , boa semana Lucas! ,boa semana joao!, boa semana Pedro! ]
+// Espera Retornar [boa semana Ana! , boa semana Lucas! ,boa semana joao!, boa semana Pedro! ] 
