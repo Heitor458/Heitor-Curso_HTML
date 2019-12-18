32e09d25fbdfbb4840edf75f3d05b6dd72886e5b
Heitor458 <heitormota16@gmail.com>
2019-12-18 18:55:31 -0300

    Adicionando arquivo atualizado 13

--- 08_js_Node/13_entrada_saida_padrao.js
+++ 08_js_Node/13_entrada_saida_padrao.js
@@ -1,4 +1,7 @@
+const endOfLine = require('os').EOL;
+
 // No inicio da aula escreva no terminal  node 13_entrada_saida_padrao.js -a 
+
 const anonimo = process.argv.indexOf('-a') !== -1
 // console.log(anonimo); // Retorna False
 
@@ -9,10 +12,9 @@ if (anonimo) {
 } else {
     process.stdout.write('Informe seu Nome:')
     // O Evento on data é desparado quando o usuario pressiona Enter
-    process.stdin.on('data', data => {  
-        const nome = data.toString().replace('\n','')
-
-        process.stdout.write(`Fala ${nome}\n`)
+    process.stdin.on('data', data => {
+        const nome = data.toString().replace(endOfLine, '') // o endOfline uma constante para caractere de nova linha esoecifico da plataforma é semelhante ao \r\n
+        process.stdout.write(`Fala  ${nome} !!`)
         process.exit()
     })
 }
