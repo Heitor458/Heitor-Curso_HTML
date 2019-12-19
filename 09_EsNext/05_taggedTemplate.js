fdae1dbd9b9851bb1ec08d69941b056d894d7fc6
Heitor458 <heitormota16@gmail.com>
2019-12-19 11:38:29 -0300

    Adicionando arquvo 05_taggedTemplate.js com conteudo da aula 147.Tagged Template #01

---
+++ 09_EsNext/05_taggedTemplate.js
@@ -0,0 +1,11 @@
+// tagged templates - processsa o template dentro de uma função 
+/* A ideia dessa funcionalidade é que você consiga processar uma template string dentro de um Função */
+function tag(partes, ...valores){
+    console.log(partes);
+    console.log(valores);
+    return 'Outra String'
+} 
+
+const aluno = 'Gui'
+const situacao = 'Aprovado'
+console.log(tag `${aluno} esta ${situacao}. `)
