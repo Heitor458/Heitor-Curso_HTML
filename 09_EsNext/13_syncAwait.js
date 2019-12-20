3f0c193f03c41de1a2c9079fb563c47b2c6e0d30
Heitor458 <heitormota16@gmail.com>
2019-12-20 16:22:36 -0300

    Adicionando arquivo 13_syncAwiat.js com conteudo da aula 155.Async/Await

---
+++ 09_EsNext/13_syncAwait.js
@@ -0,0 +1,37 @@
+const http = require('http') // mesma coisa que o require 
+
+const getTurma = (letra) => {
+    const url = `http://files.cod3r.com.br/curso-js/turma${letra}.json`
+    return new Promise((resolve, reject) => {
+        http.get(url, res => {
+            let resultado = ''
+
+            res.on('data', dados => {
+                resultado += dados
+            })
+            res.on('end', () => {
+                try {
+                    resolve(JSON.parse(resultado))
+                } catch (e) {
+                    reject(e)
+                }
+            })
+        })
+    })
+}
+
+// Recursp do ES8
+// Objetivo de Simploficar o uso de promises...
+// Ao ivés de chamar a função getTurma com o then simplesmente atribui a constante  getTurma com o await 
+
+let obterAlunos = async () => {
+    const turmaA = await getTurma('A')
+    const turmaB = await getTurma('B')
+    const turmaC = await getTurma('C')
+    return [].concat(turmaA, turmaB, turmaC)
+} // Mesmo tendo um array como retorno ele irá retornar um Objeto AsyncFunction
+
+obterAlunos()
+    .then(alunos => alunos.map(a => a.nome))
+    .then(nomes => console.log(nomes))
+    .catch(e => console.log(e))
